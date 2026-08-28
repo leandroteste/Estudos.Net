911f0b69b197d5902d0ddf8b747b13dc1e25d57a
LeandroHT <leandro.h.trindade@gmail.com>
2026-08-28 08:36:37 -0300

    feat(proj-02): Criando Projeto de cambio.

---
+++ FXPlatform/FXPlatform.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace FXPlatform.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FXPlatform/FXPlatform.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace FXPlatform.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
